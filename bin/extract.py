65b455d3e3dbdec87c5c4ee6453b812c9a8742c9
Luis Parravicini <lparravi@gmail.com>
2019-09-10 20:04:39 -0300

    output in json to a file

--- bin/extract.py
+++ bin/extract.py
@@ -3,20 +3,33 @@
 import sys
 import os
 from pathlib import Path
+import json
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import woordener
 
 
+out_file = None
+
+
 def collector(title, section):
-    # print(title, section)
-    print(title)
+    global out_file
+
+    out_file.write(json.dumps((title, section)))
+    out_file.write("\n")
+
+    print('.', end='', flush=True)
 
 
-if len(sys.argv) < 2:
-    print(f'usage: {sys.argv[0]} <wiktionary_pages_dump.xml>')
+if len(sys.argv) < 3:
+    print(f'usage: {sys.argv[0]} <wiktionary_pages_dump.xml> <output_path>')
     os.sys.exit(1)
 
-path = Path(sys.argv[1])
-woordener.extract(path, collector)
+in_path = Path(sys.argv[1])
+out_path = sys.argv[2]
+with open(out_path, 'w') as file:
+    # HACK: find a better way
+    out_file = file
+
+    woordener.extract(in_path, collector)
