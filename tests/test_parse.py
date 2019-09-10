6c9f466e662d8760781d534bfe9087545dcf72af
Luis Parravicini <lparravi@gmail.com>
2019-09-09 21:37:28 -0300

    parsing with wikitextparser

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,6 +1,6 @@
 import unittest
-import os
 from pathlib import Path
+import wikitextparser as wtp
 
 
 def load_article(path):
@@ -9,21 +9,27 @@ def load_article(path):
 
 def parse_article(path):
     data = load_article(path)
-    return data
+    doc = wtp.parse(data)
+    section = next(x for x in doc.sections if x.title == 'Dutch')
+    return section.contents
 
 
 class TestParsePage(unittest.TestCase):
 
     def setUp(self):
+        # self.maxDiff = None
         self.base_path = Path(__file__).parent.joinpath('data')
 
-    def test_only_nl_section(self):
-        article, expected = self.load_and_parse('only-nl')
+    def test_only_dutch_section(self):
+        article, expected = self.load_and_parse('only-dutch')
         self.assertEqual(article, expected)
 
     def test_several_sections(self):
         article, expected = self.load_and_parse('several-sections')
         self.assertEqual(article, expected)
+
+    def test_no_dutch_section(self):
+        self.fail("implement it")
 
     def load_and_parse(self, fname):
         path = Path(self.base_path).joinpath(fname + '.txt')
