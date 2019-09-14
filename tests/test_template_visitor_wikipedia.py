dcd7432e7faf33235ba044186e82b43eaa831b6a
Luis Parravicini <lparravi@gmail.com>
2019-09-14 15:43:49 -0300

    fixed {{wikipedia}} url

--- tests/test_template_visitor_wikipedia.py
+++ tests/test_template_visitor_wikipedia.py
@@ -14,8 +14,8 @@ class TestTemplateVisitorTitle(unittest.TestCase):
 
     def test_only_lang(self):
         lang = 'nl'
-        data = f'{{{{wikipedia|lang={lang}}}}}'
         title = 'woordenboek'
+        data = f'{{{{wikipedia|lang={lang}}}}}'
         expected = f'<a href="https://wikipedia.org/wiki/{lang}:{title}">{title}</a>'
         v = woordener.TemplateWikipediaVisitor()
         self.assertEqual(v.visit(title, data), expected)
@@ -25,6 +25,9 @@ class TestTemplateVisitorTitle(unittest.TestCase):
         title = 'lens'
         template_title = 'lens (optiek)'
         data = f'{{{{wikipedia|{template_title}|lang={lang}}}}}'
+        self.assertVisitor(title, template_title, lang, data)
+
+    def assertVisitor(self, title, template_title, lang, data)
         expected = f'<a href="https://wikipedia.org/wiki/{lang}:{template_title}">{title}</a>'
         v = woordener.TemplateWikipediaVisitor()
         self.assertEqual(v.visit(title, data), expected)
